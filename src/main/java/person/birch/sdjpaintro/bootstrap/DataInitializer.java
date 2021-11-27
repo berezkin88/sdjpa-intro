e2cf4566113cdb829cb2d518c9cb77ccb80e34d9
berezkin88 <berezkin05@gmail.com>
2021-11-27 22:45:48 +0200

    added mysql Itests

--- src/main/java/person/birch/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/person/birch/sdjpaintro/bootstrap/DataInitializer.java
@@ -1,11 +1,13 @@
 package person.birch.sdjpaintro.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import person.birch.sdjpaintro.domain.Book;
 import person.birch.sdjpaintro.repositories.BookRepository;
 
 @Component
+@Profile({"local", "default"})
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
@@ -16,6 +18,7 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
 
         var bookDDD = new Book("Domain Driven Design", "1234", "RandomHouse");
         var bookSIP = new Book("Spring in Action", "2345", "Oriely");
---
+++ src/test/java/person/birch/sdjpaintro/MySqlIntegrationTest.java
@@ -0,0 +1,28 @@
+package person.birch.sdjpaintro;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import person.birch.sdjpaintro.repositories.BookRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ComponentScan(basePackages = {"person.birch.sdjpaintro.bootstrap"})
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MySqlIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSpliceTransaction() {
+        var countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+    }
+}
